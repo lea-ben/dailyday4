398fe3648eeb7a4b20362f6b3def115a60859845
lea-ben <leabenillouche@MacBook-Pro-de-Lea.local>
2021-01-20 21:26:06 +0200

    first

---
+++ dailychallenge.js
@@ -0,0 +1,16 @@
+// Ask a user for several words (separated by commas).
+
+function stars () {
+let words= prompt ("please,tell me something"); 
+
+let arrayWord = words.split(",");
+
+for (let words1 of arrayWord) 
+{ 
+
+console.log ( ("**********") + words + (" "));
+
+
+stars(prompt(''));
+
+}}
